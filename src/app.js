16ee96c26fdf0acefada820f427abe3d40748323
Pablo Ballesteros Suarez <132361283+PabloBalle@users.noreply.github.com>
2023-06-14 20:04:25 +0000

    V1.0 Domain Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,41 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["joggeres", "racom"];
+var prefix = [".com", ".es", ".org", ".net", ".org"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// Función para generar las combinaciones de nombres de dominio
+function generateDomainNames(pronoun, adj, noun, prefix) {
+  var domainNames = [];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  // Recorrer todas las listas de pronombres, adjetivos y sustantivos
+  for (var i = 0; i < pronoun.length; i++) {
+    for (var j = 0; j < adj.length; j++) {
+      for (var k = 0; k < noun.length; k++) {
+        for (var l = 0; l < prefix.length; l++) {
+          if (noun[k].endsWith("es") && prefix[l] === ".es") {
+            // Combinar los elementos de las listas y agregar el sufijo ".com"
+            var cleanNoun = noun[k].slice(0, -2);
+            // Combinar los elementos de las listas sin "com" y agregar el sufijo ".com"
+            var domainName = pronoun[i] + adj[j] + cleanNoun + prefix[l];
+            domainNames.push(domainName);
+          } else if (noun[k].endsWith("com") && prefix[l] === ".com") {
+            // Combinar los elementos de las listas y agregar el sufijo ".com"
+            cleanNoun = noun[k].slice(0, -3);
+            // Combinar los elementos de las listas sin "com" y agregar el sufijo ".com"
+            domainName = pronoun[i] + adj[j] + cleanNoun + prefix[l];
+            domainNames.push(domainName);
+          } else {
+            domainName = pronoun[i] + adj[j] + noun[k] + prefix[l];
+            domainNames.push(domainName);
+          }
+        }
+      }
+    }
+  }
+
+  return domainNames;
+}
+
+// Generar las combinaciones de nombres de dominio
+var domainNames = generateDomainNames(pronoun, adj, noun, prefix);
+consol.log();
